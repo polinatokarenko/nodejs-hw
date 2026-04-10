bf2e9f2675cad2248ed718c6cb9e99efa191b941
Yurin <yuriinnx@gmail.com>
2026-04-10 23:16:36 +0300

    do the part of creating a model

---
+++ src/models/note.js
@@ -0,0 +1,27 @@
+import { Schema } from "mongoose";
+import { model } from "mongoose";
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      default: false,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      default: 'Todo',
+      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Note = model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -1,27 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+import { logger } from './middleware/logger';
+import { notFoundHandler } from './middleware/notFoundHandler';
+import { errorHandler } from './middleware/errorHandler';
+import { connectMongoDB } from './db/connectMongoDB';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(logger);
 app.use(express.json());
 app.use(cors());
-app.use(pino({
-  level: 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid, hostname',
-      messageFormat: '{req.method} {req.url} {res.statusCode} {responseTime}ms',
-      hideObject: true,
-    }
-  }
-}),
-);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ "message": "Retrieved all notes" });
@@ -35,13 +25,11 @@ app.get('/test-error', () => {
   throw new Error('Simulated server error');
 });
 
-app.use((req, res) => {
-  res.status(404).json({ "message": "Route not found" });
-});
+app.use(notFoundHandler);
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ "message": err.message});
-});
+app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
