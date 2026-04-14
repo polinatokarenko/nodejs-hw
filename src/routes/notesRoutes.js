4f5415ec24be035a95dedb301a3e2bf01cb70081
Yurin <yuriinnx@gmail.com>
2026-04-15 00:46:55 +0300

    do the whole branch

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,20 +1,27 @@
 import { Router } from "express";
+import { celebrate } from "celebrate";
+
 import { getAllNotes } from "../controllers/notesController.js";
 import { getNoteById } from "../controllers/notesController.js";
 import { createNote } from "../controllers/notesController.js";
 import { deleteNote } from "../controllers/notesController.js";
 import { updateNote } from "../controllers/notesController.js";
 
+import { getAllNotesSchema } from "../validations/notesValidation.js";
+import { noteIdSchema } from "../validations/notesValidation.js";
+import { createNoteSchema } from "../validations/notesValidation.js";
+import { updateNoteSchema } from "../validations/notesValidation.js";
+
 const router = Router();
 
-router.get('/notes', getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 
-router.get('/notes/:noteId', getNoteById);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 
-router.post('/notes', createNote);
+router.post('/notes', celebrate(createNoteSchema), createNote);
 
-router.delete('/notes/:noteId', deleteNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 
-router.patch('/notes/:noteId', updateNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
