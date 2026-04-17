1c9070b7548600b8cdd5018678d441b58c8cbb09
Yurin <yuriinnx@gmail.com>
2026-04-17 16:58:19 +0300

    add creating and setting cookies

---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+import { Schema } from "mongoose";
+import { model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+userSchema.pre('save', function () {
+  if (!this.username) {
+    this.username = this.email;
+  }
+});
+
+export const User = model('User', userSchema);
