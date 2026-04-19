015b782a9c39bc8a33f2c1d648c2b476b9c09a6f
Yurin <yuriinnx@gmail.com>
2026-04-19 22:18:46 +0300

    add email settings

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,11 @@ import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
 import { createSession, setSessionCookies } from "../services/auth.js";
 import { Session } from "../models/session.js";
+import jwt from 'jsonwebtoken';
+import path from 'node:path';
+import fs from 'node:fs';
+import handlebars from "handlebars";
+import { sendEmail } from "../utils/sendMail.js";
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -84,4 +89,41 @@ export const logoutUser = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+};
+
+export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(200).json({ message: 'Password reset email sent successfully' });
+  };
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html,
+    });
+  } catch {
+    throw createHttpError(500, 'Failed to send the email, please try again later.');
+  };
+
+  res.status(200).json({ message: 'Password reset email sent successfully' });
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
 
-import { loginUserSchema, registerUserSchema } from "../validations/authValidation.js";
-import { loginUser, logoutUser, refreshUserSession, registerUser } from "../controllers/authController.js";
+import { loginUserSchema, registerUserSchema, requestResetEmailSchema } from "../validations/authValidation.js";
+import { loginUser, logoutUser, refreshUserSession, registerUser, requestResetEmail } from "../controllers/authController.js";
 
 const authRouter = Router();
 
@@ -13,5 +13,7 @@ authRouter.post('/auth/login', celebrate(loginUserSchema), loginUser);
 authRouter.post('/auth/refresh', refreshUserSession);
 
 authRouter.post('/auth/logout', logoutUser);
+
+authRouter.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
 
 export default authRouter;
